bdd900854f98d33b78a3821bc28f071e13918083
Partha Gundarpi <buzzpartha@gmail.com>
2023-03-19 15:36:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if(fruit_choice_1):
 
 def insert_row_snowflake (new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute ("insert into fruit_load_list values ('"+new_fruit +"')")
+    my_cur.execute ("insert into pc_rivery_db.public.fruit_load_list values ('"+new_fruit +"')")
     return "Thanks for adding "+new_fruit
 
 
